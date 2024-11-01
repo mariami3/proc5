29fdef9931730073e047a0caad84b8c570d53647
mariami3 <mariami_petriashvili06@mail.ru>
2024-11-01 12:48:14 +0300

    proc5

---
+++ app/src/main/java/com/example/proc5/ThirdFragment.java
@@ -0,0 +1,22 @@
+package com.example.proc5;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ThirdFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view=inflater.inflate(R.layout.fragment_third, container, false);
+        view.setBackgroundResource(R.color.aqua);
+        return view;
+    }
+
+}
